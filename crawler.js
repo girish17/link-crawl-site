05dae8f2bc69cf2cfaa8d0437f98a062411f7bf2
girish17 <girishm@mailfence.com>
2019-10-09 19:51:17 +0530

    removing max request

--- crawler.js
+++ crawler.js
@@ -4,8 +4,6 @@ var URL = require('url-parse');
 
 var START_URL = "https://medium.com";
 var MAX_PAGES_TO_VISIT = 100;
-var MAX_REQUEST = 5;
-var requests_made = 0;
 var pagesVisited = {};
 var numPagesVisited = 0;
 var pagesToVisit = [];
@@ -22,37 +20,34 @@ function crawl() {
     }
     var nextPage = pagesToVisit.pop();
     if (nextPage in pagesVisited) {
-        // We've already visited this page, so repeat the crawl
-        requests_made = 0;
+
         crawl();
     } else {
-        // New page we haven't visited
-        /*if(requests_made < MAX_REQUEST)*/ {
+
             requests_made++;
             visitPage(nextPage, crawl);
-        }
+        
     }
 }
 
 function visitPage(url, callback) {
-    // Add page to our set
+
     pagesVisited[url] = true;
     numPagesVisited++;
 
-    // Make the request
     console.log("Visiting page " + url);
     request(url, function (error, response, body) {
-        // Check status code (200 is HTTP OK)
+   
         console.log("Status code: " + response.statusCode);
         if (response.statusCode !== 200) {
             callback();
             return;
         }
         else {
-            // Parse the document body
+   
             var $ = cheerio.load(body);
             collectInternalLinks($);
-            // In this short program, our callback is just calling crawl()
+ 
             callback();
         }
     });
